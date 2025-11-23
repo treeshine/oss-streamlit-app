a963573ed7b88867a112c8c2557618185e97520f
LeeDongHyeuk <treeshine915@gmail.com>
2025-11-23 22:42:38 +0900

    add title

--- app/task5.py
+++ app/task5.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Task 5: 파일 업로드")
+
+
 def load_csv():
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     if uploaded_file is not None:
         # Read the CSV file into a Pandas DataFrame
         return pd.read_csv(uploaded_file)
     return None
+
+
+data = load_csv()
